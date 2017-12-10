b37066d4efb3c3e6df138b3e1fa68f49b778d64a
Alejandro Ranchal Pedrosa <a.ranchalpedrosa@gmail.com>
2017-12-10 11:54:51 +0100

    last commit before presentation

--- truffle.js
+++ truffle.js
@@ -1,4 +1,20 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  rpc: {
+    host: 'localhost',
+    port: '8545'
+ },
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      host: "localhost", // Connect to geth on the specified
+      port: 8545,
+      from: "0x45a6b16e1abd4e3786d39eb11214ba1fe57c5f48", // default address to use for any transaction Truffle makes during migrations
+      network_id: 4,
+      gas: 4612388 // Gas limit used for deploys
+    }
+  },
 };
