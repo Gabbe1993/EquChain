a3cd6ab71241837a6ee149106c7316eaa35f181a
Gabriel Vilen <gabriel.vilen@gmail.com>
2018-02-07 12:45:17 +0100

    init market branch

--- contracts/TradingEmissions.sol
+++ contracts/TradingEmissions.sol
@@ -15,6 +15,7 @@ contract mortal {
 contract tradingEmissions is mortal {
 
   address creator;
+  
   address regulator;
   // STATE 
   struct Company{
@@ -26,6 +27,8 @@ contract tradingEmissions is mortal {
     uint fine; // if >0 then it is locked
     uint fine_deadline;
   }
+
+  address[] public market;
   
   uint emusPrice = 1; //1 wei == 1 emissionsunitPrice
   mapping (address => Company) companies;
@@ -34,7 +37,7 @@ contract tradingEmissions is mortal {
     creator = msg.sender;
     addCompany(msg.sender, "testcompany", 500);
   }
-
+  
   modifier isCompany(address ad){
     require(bytes(companies[ad].name).length != 0);//name is the ID atm
     _;
@@ -47,17 +50,44 @@ contract tradingEmissions is mortal {
     _;
   }
   
-  function buyEmus(address seller, uint emus) public payable // TODO QUEUE for buying instead of specifying seller
-    isCompany(seller)
-    isCompany(msg.sender)
-    enoughFreeUnits(seller,emus,msg.value)
-      {
-	companies[seller].emusOnSale-= emus;
-	companies[msg.sender].emus-= int(emus);
-	
-
-	seller.send(msg.value);//TO-TEST send money to seller
+  function buyEmusFromMarket(uint emus) public payable {
+    for(uint i = 0; i < market.length; i++) {
+      if(emus <= 0) { // if all emus is bought we return
+        return;
       }
+      emus -= buyEmus(msg.sender, market[i], emus, msg.value); // buy and decrease emus
+    }
+
+  }
+
+  function removeCompanyFromMarket(address company) public payable {
+    for(uint i = 0; i < market.length; i++) {
+      if(market[i] == company) {
+        delete market[i];
+    //    delete companies[company];
+      }
+    }
+  }
+  
+  function buyEmus(address buyerAd, address sellerAd, uint emus, uint value) 
+    isCompany(sellerAd)
+    isCompany(buyerAd) // msg.sender = buyer
+    enoughFreeUnits(sellerAd, emus, value) // msg.value
+    returns (uint)
+  {
+    Company seller = companies[sellerAd];
+    Company buyer = companies[buyerAd]; // TODO: check if gets reference or value
+
+	  seller.emusOnSale -= emus;
+	  buyer.emus -= int(emus); // emus get decreased when buying
+	
+	  sellerAd.send(value);//TO-TEST send money to seller
+
+    if (seller.emusOnSale == 0) {
+      removeCompanyFromMarket(sellerAd); 
+    }
+    return emus;
+  }
   
   modifier enoughForSale(address owner, uint emusToSale){
     require(companies[owner].emuLimit>=companies[owner].emusOnSale+emusToSale);
@@ -140,9 +170,10 @@ contract tradingEmissions is mortal {
   
   function addCompany(address company, string name, uint emuLimit)
     isCreator(msg.sender)
-    isNotCompany(company){
+    isNotCompany(company) {
     companies[company].name=name;
     companies[company].emuLimit=emuLimit;
+    market.push(company); // add company to market
   }
 
   function addRegulator(address newRegulator)
@@ -153,28 +184,60 @@ contract tradingEmissions is mortal {
 
   // Getters
   
-  function getName() isCompany(msg.sender) view public returns (string)
-  {
+  function getName() view public returns (string) {
     return companies[msg.sender].name;
   }
+  
+    
+  function getCompanyName(address adr) view public returns (string) {
+    return companies[adr].name;
+  }
 
-  function getEmus() isCompany(msg.sender) view public returns (int)   
-  {
+
+  function getEmus() view public returns (int) {
     return companies[msg.sender].emus;
   }
     
-  function getEmuLimit()  isCompany(msg.sender) view public returns (uint)   
-  {
+  function getEmuLimit() view public returns (uint) {
     return companies[msg.sender].emuLimit;
   }
 
-  function getEmusOnSale() isCompany(msg.sender) view public returns (bytes32) {
-    return bytes32(companies[msg.sender].emusOnSale);
+  function getEmusOnSale() view public returns (uint) {
+    return companies[msg.sender].emusOnSale;
   }
 
-  function getFine()  isCompany(msg.sender) view public returns (uint)   
-  {
-    return companies[msg.sender].fine;
+  function getFine() view public returns (string) {
+    return bytes32ToString(uintToBytes(companies[msg.sender].fine));
+  }  
+
+  function bytes32ToString(bytes32 x) constant returns (string) {
+      bytes memory bytesString = new bytes(32);
+      uint charCount = 0;
+      for (uint j = 0; j < 32; j++) {
+          byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
+          if (char != 0) {
+              bytesString[charCount] = char;
+              charCount++;
+          }
+      }
+      bytes memory bytesStringTrimmed = new bytes(charCount);
+      for (j = 0; j < charCount; j++) {
+          bytesStringTrimmed[j] = bytesString[j];
+      }
+      return string(bytesStringTrimmed);
   }
-  
+
+  function uintToBytes(uint v) constant returns (bytes32 ret) {
+    if (v == 0) {
+        ret = '0';
+    }
+    else {
+        while (v > 0) {
+            ret = bytes32(uint(ret) / (2 ** 8));
+            ret |= bytes32(((v % 10) + 48) * 2 ** (8 * 31));
+            v /= 10;
+        }
+    }
+    return ret;
+}
 }
